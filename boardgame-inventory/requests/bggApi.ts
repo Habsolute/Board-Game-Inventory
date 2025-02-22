49ec0660bc60b1a54f35f178d556aadddd3d63eb
JMB <jean-marc-boisvert@hotmail.com>
2025-02-22 15:49:42 -0500

    create api call and all request to BGG API

---
+++ boardgame-inventory/requests/bggApi.ts
@@ -0,0 +1,24 @@
+import { parseString } from "xml2js";
+import { promisify } from "util";
+import { BGGGameDetails, BGGSearchResult } from "utils/types/boardgame";
+
+const BGG_API_BASE = "https://boardgamegeek.com/xmlapi2";
+const parseXMLPromise = promisify<string, BGGSearchResult | BGGGameDetails>(
+  parseString
+);
+
+export async function searchBoardGame(query: string): Promise<BGGSearchResult> {
+  const response = await fetch(
+    `${BGG_API_BASE}/search?query=${encodeURIComponent(query)}&type=boardgame`
+  );
+  const xmlData = await response.text();
+  return parseXMLPromise(xmlData) as Promise<BGGSearchResult>;
+}
+
+export async function getBoardGameDetails(
+  gameId: string
+): Promise<BGGGameDetails> {
+  const response = await fetch(`${BGG_API_BASE}/thing?id=${gameId}&stats=1`);
+  const xmlData = await response.text();
+  return parseXMLPromise(xmlData) as Promise<BGGGameDetails>;
+}
